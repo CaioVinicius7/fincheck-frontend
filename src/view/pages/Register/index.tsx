8bd5c40fdab80bc0cb6ac07f9f96d2707e140342
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-02 16:09:45 -0300

    feat: :lipstick: Página Register

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,3 +1,38 @@
+import { Link } from "react-router-dom";
+
+import { Button } from "../../Components/Button";
+import { Input } from "../../Components/Input";
+
 export function Register() {
-  return <h1>Register Page</h1>;
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold tracking-[-1px] text-gray-900">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="tracking-[-0.5px] text-gray-700">
+            Já possui uma conta?
+          </span>
+          <Link
+            to="/login"
+            className="font-medium tracking-[-0.5px] text-teal-900"
+          >
+            Fazer Login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input type="text" name="name" placeholder="Nome" />
+        <Input type="email" name="email" placeholder="E-mail" />
+        <Input type="password" name="password" placeholder="Senha" />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
+  );
 }
