c4affdff86cce2a4c164b6f44485b619f0e5b7c1
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-03 21:30:15 -0300

    refactor: :recycle: Utilizando função utilitária cn

--- src/view/Components/Button.tsx
+++ src/view/Components/Button.tsx
@@ -1,5 +1,6 @@
 import type { ComponentProps } from "react";
-import { twMerge } from "tailwind-merge";
+
+import { cn } from "../../app/utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
@@ -7,7 +8,7 @@ export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className={twMerge(
+      className={cn(
         "h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white outline-none transition-colors hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
         className
       )}
