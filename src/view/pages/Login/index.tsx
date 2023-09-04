03fc0955ae0a880d18e3998d6e7e4b5e575844e6
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-03 21:34:09 -0300

    feat: :sparkles: Integração do formulário de login com o React Hook Form e Zod

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 import { Button } from "../../Components/Button";
 import { Input } from "../../Components/Input";
+import { useLoginController } from "./useLoginController";
 
 export function Login() {
+  const { handleSubmit, register, errors } = useLoginController();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4 text-center">
@@ -24,9 +27,19 @@ export function Login() {
         </p>
       </header>
 
-      <form className="mt-[60px] flex flex-col gap-4">
-        <Input type="email" name="email" placeholder="E-mail" />
-        <Input type="password" name="password" placeholder="Senha" />
+      <form onSubmit={handleSubmit} className="mt-[60px] flex flex-col gap-4">
+        <Input
+          type="email"
+          placeholder="E-mail"
+          error={errors.email?.message}
+          {...register("email")}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          error={errors.password?.message}
+          {...register("password")}
+        />
 
         <Button type="submit" className="mt-2">
           Entrar
