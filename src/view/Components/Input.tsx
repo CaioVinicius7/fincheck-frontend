b8771984ccb28a9151853e2e9161d5d1e4915424
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-03 21:32:57 -0300

    feat: :sparkles: Estado de erro no componente Input

--- src/view/Components/Input.tsx
+++ src/view/Components/Input.tsx
@@ -1,27 +1,49 @@
-import type { ComponentProps } from "react";
+import { forwardRef, type ComponentProps } from "react";
+
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+
+import { cn } from "../../app/utils/cn";
 
 interface InputProps extends ComponentProps<"input"> {
   name: string;
+  error?: string;
 }
 
-export function Input({ name, placeholder, id, ...props }: InputProps) {
-  const inputId = id ?? name;
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ name, error, placeholder, id, className, ...props }, ref) => {
+    const inputId = id ?? name;
 
-  return (
-    <div className="relative">
-      <input
-        {...props}
-        id={inputId}
-        className="peer h-[52px] w-full rounded-lg border border-gray-500 bg-white px-3 pt-4 text-gray-800 outline-none transition-all placeholder-shown:pt-0 focus:border-gray-800"
-        placeholder=" "
-      />
+    return (
+      <div className="relative">
+        <input
+          {...props}
+          id={inputId}
+          name={name}
+          ref={ref}
+          placeholder=" "
+          className={cn(
+            "peer h-[52px] w-full rounded-lg border border-gray-500 bg-white px-3 pt-4 text-gray-800 outline-none transition-all placeholder-shown:pt-0 focus:border-gray-800",
+            !!error && "!border-red-900",
+            className
+          )}
+        />
 
-      <label
-        htmlFor={inputId}
-        className="pointer-events-none absolute left-[13px] top-2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base"
-      >
-        {placeholder}
-      </label>
-    </div>
-  );
-}
+        <label
+          htmlFor={inputId}
+          className="pointer-events-none absolute left-[13px] top-2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base"
+        >
+          {placeholder}
+        </label>
+
+        {!!error && (
+          <div className="mt-2 flex items-center gap-2 text-red-900">
+            <CrossCircledIcon />
+            <span className="text-xs">{error}</span>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
