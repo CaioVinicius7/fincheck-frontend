2e8e16c51e0bec84dfa22e1759653114469d5c11
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-07 19:11:06 -0300

    feat: :sparkles: Estado de carregamento do componente Button

--- src/view/Components/Button.tsx
+++ src/view/Components/Button.tsx
@@ -1,17 +1,29 @@
 import type { ComponentProps } from "react";
 
 import { cn } from "../../app/utils/cn";
+import { Spinner } from "./Spinner";
 
-interface ButtonProps extends ComponentProps<"button"> {}
+interface ButtonProps extends ComponentProps<"button"> {
+  isLoading?: boolean;
+}
 
-export function Button({ className, ...props }: ButtonProps) {
+export function Button({
+  isLoading,
+  disabled,
+  className,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
+      disabled={isLoading || disabled}
       className={cn(
-        "h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white outline-none transition-colors hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
+        "flex h-12 items-center justify-center rounded-2xl bg-teal-900 px-6 font-medium text-white outline-none transition-colors hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
         className
       )}
-    />
+    >
+      {!isLoading ? children : <Spinner className="h-6 w-6" />}
+    </button>
   );
 }
