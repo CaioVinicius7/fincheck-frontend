118640f013ce569ba32cfc019e9b95745a26b17c
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-11 18:38:44 -0300

    feat: :wrench: Alterando configuração do React Query

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./app/contexts/AuthContext";
 import { Router } from "./Router";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 export function App() {
   return (
