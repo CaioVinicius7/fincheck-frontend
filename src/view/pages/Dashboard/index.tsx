2128a7b02110d5e9350e6f782be590cbfe4a35fc
Caio Vinícius <caio1525pereira@gmail.com>
2023-09-17 06:53:57 -0300

    feat: :lipstick: Header da página Dashboard

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,3 +1,15 @@
+import { Logo } from "../../Components/Logo";
+import { UserMenu } from "../../Components/UserMenu";
+
 export function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  return (
+    <div className="flex h-full w-full flex-col px-8 pb-8 pt-6">
+      <header className="flex h-12 items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+        <UserMenu />
+      </header>
+
+      <main>content</main>
+    </div>
+  );
 }
