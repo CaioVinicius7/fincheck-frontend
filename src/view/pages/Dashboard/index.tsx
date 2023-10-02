c916780fdf7db19c2f44ae71fa73b0976d829ad6
Caio Vinícius <caio1525pereira@gmail.com>
2023-10-02 03:03:45 -0300

    feat: :lipstick: Estrutura da página Dashboard

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,15 +1,24 @@
 import { Logo } from "../../Components/Logo";
 import { UserMenu } from "../../Components/UserMenu";
+import { Accounts } from "./components/Accounts";
+import { Transactions } from "./components/Transactions";
 
 export function Dashboard() {
   return (
-    <div className="flex h-full w-full flex-col px-8 pb-8 pt-6">
+    <div className="flex h-full w-full flex-col p-4 md:px-8 md:pb-8 md:pt-6">
       <header className="flex h-12 items-center justify-between">
         <Logo className="h-6 text-teal-900" />
         <UserMenu />
       </header>
 
-      <main>content</main>
+      <main className="flex flex-1 flex-col gap-4 md:flex-row">
+        <div className="w-full md:w-1/2">
+          <Accounts />
+        </div>
+        <div className="w-full md:w-1/2">
+          <Transactions />
+        </div>
+      </main>
     </div>
   );
 }
