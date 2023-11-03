dc6ecf0d52c6c24bf81b2f2aaa9202018e1df962
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-03 16:33:19 -0300

    chore: :wrench: Configuração do path mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
@@ -6,5 +7,22 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  resolve: {
+    alias: {
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+
+      "@config": path.resolve(__dirname, "./src/app/config"),
+      "@contexts": path.resolve(__dirname, "./src/app/contexts"),
+      "@hooks": path.resolve(__dirname, "./src/app/hooks"),
+      "@services": path.resolve(__dirname, "./src/app/services"),
+      "@utils": path.resolve(__dirname, "./src/app/utils"),
+      "@entities": path.resolve(__dirname, "./src/app/entities"),
+
+      "@components": path.resolve(__dirname, "./src/view/Components"),
+      "@layouts": path.resolve(__dirname, "./src/view/layouts"),
+      "@pages": path.resolve(__dirname, "./src/view/pages")
+    }
   }
 });
