52268a2144f1bd3240d06669928627b6a2d58c41
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-15 16:10:26 -0300

    refactor: :lipstick: Adição de borda ao componente UserMenu

--- src/view/Components/UserMenu.tsx
+++ src/view/Components/UserMenu.tsx
@@ -1,6 +1,6 @@
 export function UserMenu() {
   return (
-    <div className="flex h-12 w-12 items-center justify-center rounded-full border-teal-100 bg-teal-50">
+    <div className="flex h-12 w-12 items-center justify-center rounded-full border border-teal-100 bg-teal-50">
       <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
         CV
       </span>
