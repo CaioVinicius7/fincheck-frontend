1603282d29d3b8ad35843d89eaa1aeb6279101b6
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-15 18:54:51 -0300

    feat: :sparkles: Contexto do dashboard

---
+++ src/view/pages/Dashboard/DashboardContext/useDashboard.tsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+
+import { DashboardContext } from ".";
+
+export function useDashboard() {
+  const context = useContext(DashboardContext);
+
+  return context;
+}
--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -3,23 +3,26 @@ import { UserMenu } from "@components/UserMenu";
 import { Logo } from "../../Components/Logo";
 import { Accounts } from "./components/Accounts";
 import { Transactions } from "./components/Transactions";
+import { DashboardProvider } from "./DashboardContext";
 
 export function Dashboard() {
   return (
-    <div className="flex h-full w-full flex-col gap-4 p-4 md:px-8 md:pb-8 md:pt-6">
-      <header className="flex h-12 items-center justify-between">
-        <Logo className="h-6 text-teal-900" />
-        <UserMenu />
-      </header>
+    <DashboardProvider>
+      <div className="flex h-full w-full flex-col gap-4 p-4 md:px-8 md:pb-8 md:pt-6">
+        <header className="flex h-12 items-center justify-between">
+          <Logo className="h-6 text-teal-900" />
+          <UserMenu />
+        </header>
 
-      <main className="flex max-h-[calc(100%-16px-32px)] flex-1 flex-col gap-4 md:flex-row">
-        <div className="w-full md:w-1/2">
-          <Accounts />
-        </div>
-        <div className="w-full md:w-1/2">
-          <Transactions />
-        </div>
-      </main>
-    </div>
+        <main className="flex max-h-[calc(100%-16px-32px)] flex-1 flex-col gap-4 md:flex-row">
+          <div className="w-full md:w-1/2">
+            <Accounts />
+          </div>
+          <div className="w-full md:w-1/2">
+            <Transactions />
+          </div>
+        </main>
+      </div>
+    </DashboardProvider>
   );
 }
