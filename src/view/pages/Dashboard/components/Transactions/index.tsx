37eedf09f977ab3d9339aeea996d8e3a23865966
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-15 04:23:35 -0300

    feat: :lipstick: Cabeçalho da seção de transações

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,7 +1,28 @@
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
+import { FilterIcon } from "@components/icons/FilterIcon";
+import { TransactionsIcon } from "@components/icons/TransactionsIcon";
+
 export function Transactions() {
   return (
     <div className="h-full w-full rounded-2xl bg-gray-100 px-4 py-8 md:p-10">
-      Transactions
+      <header>
+        <div className="flex items-center justify-between">
+          <button className="flex items-center gap-2">
+            <TransactionsIcon />
+            <span className="text-sm font-medium tracking-[-0.5px] text-gray-800">
+              Transações
+            </span>
+            <ChevronDownIcon className="text-gray-900" />
+          </button>
+
+          <button>
+            <FilterIcon />
+          </button>
+        </div>
+      </header>
+
+      <div className="mt-4">Content</div>
     </div>
   );
 }
