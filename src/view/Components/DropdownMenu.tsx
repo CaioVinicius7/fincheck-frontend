9390cffc5a65ad18736f79bd84e9c161a132b805
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-29 12:55:45 -0300

    feat: :lipstick: Componente DropdownMenu

---
+++ src/view/Components/DropdownMenu.tsx
@@ -0,0 +1,70 @@
+import * as RdxDropdownMenu from "@radix-ui/react-dropdown-menu";
+import type { ReactNode } from "react";
+
+import { cn } from "@utils/cn";
+
+function DropdownMenuRoot({ children }: { children: ReactNode }) {
+  return <RdxDropdownMenu.Root>{children}</RdxDropdownMenu.Root>;
+}
+
+function DropdownMenuTrigger({ children }: { children: ReactNode }) {
+  return (
+    <RdxDropdownMenu.Trigger className="outline-none">
+      {children}
+    </RdxDropdownMenu.Trigger>
+  );
+}
+
+interface DropdownMenuContentProps {
+  children: ReactNode;
+  className?: string;
+}
+
+function DropdownMenuContent({
+  children,
+  className
+}: DropdownMenuContentProps) {
+  return (
+    <RdxDropdownMenu.Portal>
+      <RdxDropdownMenu.Content
+        className={cn(
+          "space-x-2 rounded-2xl bg-white p-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
+          className
+        )}
+      >
+        {children}
+      </RdxDropdownMenu.Content>
+    </RdxDropdownMenu.Portal>
+  );
+}
+
+interface DropdownMenuItemProps {
+  children: ReactNode;
+  className?: string;
+  onSelect?: () => void;
+}
+
+function DropdownMenuItem({
+  children,
+  className,
+  onSelect
+}: DropdownMenuItemProps) {
+  return (
+    <RdxDropdownMenu.Item
+      onSelect={onSelect}
+      className={cn(
+        "flex min-h-[48px] cursor-pointer items-center rounded-2xl p-4 text-sm text-gray-800 outline-none transition-colors data-[highlighted]:bg-gray-50",
+        className
+      )}
+    >
+      {children}
+    </RdxDropdownMenu.Item>
+  );
+}
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem
+};
