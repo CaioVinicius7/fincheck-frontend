46b37806dcbd3445a22b404b1629c88f443ffbad
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-29 13:07:46 -0300

    feat: :sparkles: Dropdown do UserMenu

--- src/view/Components/UserMenu.tsx
+++ src/view/Components/UserMenu.tsx
@@ -1,9 +1,31 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+
+import { useAuth } from "@hooks/useAuth";
+
+import { DropdownMenu } from "./DropdownMenu";
+
 export function UserMenu() {
+  const { signOut } = useAuth();
+
   return (
-    <div className="flex h-12 w-12 items-center justify-center rounded-full border border-teal-100 bg-teal-50">
-      <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
-        CV
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="flex h-12 w-12 items-center justify-center rounded-full border border-teal-100 bg-teal-50">
+          <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
+            CV
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          onSelect={signOut}
+          className="flex items-center justify-between"
+        >
+          Sair
+          <ExitIcon className="h-4 w-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
