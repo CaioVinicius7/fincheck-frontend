b4e85128e9225e7bc36805710abb327ee9f0084e
Caio Vinícius <caio1525pereira@gmail.com>
2023-11-29 13:25:50 -0300

    refactor: :recycle: Organização dos imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import { AuthProvider } from "@contexts/AuthContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Router } from "./Router";
 
