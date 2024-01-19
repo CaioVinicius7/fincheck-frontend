19dd45979948c8c0286cb9edf36673407db8ac91
Caio Vinícius <caio1525pereira@gmail.com>
2024-01-19 03:22:05 -0300

    feat: :lipstick: Variantes do componente Button

--- src/view/Components/Button.tsx
+++ src/view/Components/Button.tsx
@@ -6,6 +6,7 @@ import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ComponentProps<"button"> {
   isLoading?: boolean;
+  variant?: "danger" | "ghost";
 }
 
 export function Button({
@@ -13,6 +14,7 @@ export function Button({
   disabled,
   className,
   children,
+  variant,
   ...props
 }: ButtonProps) {
   return (
@@ -21,6 +23,9 @@ export function Button({
       disabled={isLoading || disabled}
       className={cn(
         "flex h-12 items-center justify-center rounded-2xl bg-teal-900 px-6 font-medium text-white outline-none transition-colors hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
+        variant === "danger" && "bg-red-900 hover:bg-red-800",
+        variant === "ghost" &&
+          "border border-gray-800 bg-transparent text-gray-800 hover:bg-gray-800/5",
         className
       )}
     >
