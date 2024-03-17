0f129f0dcfaf3d6e7a355ab6cea670c2b4a0cb2d
Caio Vinícius <caio1525pereira@gmail.com>
2024-03-17 14:50:44 -0300

    feat: :sparkles: Custom hook useTransactions

---
+++ src/app/hooks/useTransactions.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { transactionsService } from "@services/transactionsService";
+
+export function useTransactions() {
+  const { data, isFetching, isInitialLoading } = useQuery({
+    queryKey: ["transactions"],
+    queryFn: () =>
+      transactionsService.getAll({
+        month: 2,
+        year: 2024
+      })
+  });
+
+  return {
+    transactions: data ?? [],
+    isLoading: isFetching,
+    isInitialLoading
+  };
+}
