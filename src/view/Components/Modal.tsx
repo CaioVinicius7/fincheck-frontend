e8dbca22a39fc76e5b752bf4eb7546442c49876d
Caio Vinícius <caio1525pereira@gmail.com>
2024-03-20 21:27:25 -0300

    refactor: :recycle: Renderizando modal dentro do portal

--- src/view/Components/Modal.tsx
+++ src/view/Components/Modal.tsx
@@ -28,31 +28,31 @@ export function Modal({
             "data-[state=open]:animate-overlay-show"
           )}
         />
+
+        <RdxDialog.Content
+          className={cn(
+            "fixed left-1/2 top-1/2 z-[51] w-full max-w-[400px] -translate-x-1/2 -translate-y-1/2 space-y-10 rounded-2xl bg-white p-6 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] outline-none",
+            "data-[state=open]:animate-content-show"
+          )}
+        >
+          <header className="flex h-12 items-center justify-between text-gray-800">
+            <button
+              onClick={onClose}
+              className="flex h-12 w-12 items-center justify-center outline-none"
+            >
+              <Cross2Icon className="h-6 w-6" />
+            </button>
+
+            <span className="text-lg font-bold tracking-[-1px]">{title}</span>
+
+            <div className="flex h-12 w-12 items-center justify-center">
+              {rightAction}
+            </div>
+          </header>
+
+          <div>{children}</div>
+        </RdxDialog.Content>
       </RdxDialog.Portal>
-
-      <RdxDialog.Content
-        className={cn(
-          "fixed left-1/2 top-1/2 z-[51] w-full max-w-[400px] -translate-x-1/2 -translate-y-1/2 space-y-10 rounded-2xl bg-white p-6 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] outline-none",
-          "data-[state=open]:animate-content-show"
-        )}
-      >
-        <header className="flex h-12 items-center justify-between text-gray-800">
-          <button
-            onClick={onClose}
-            className="flex h-12 w-12 items-center justify-center outline-none"
-          >
-            <Cross2Icon className="h-6 w-6" />
-          </button>
-
-          <span className="text-lg font-bold tracking-[-1px]">{title}</span>
-
-          <div className="flex h-12 w-12 items-center justify-center">
-            {rightAction}
-          </div>
-        </header>
-
-        <div>{children}</div>
-      </RdxDialog.Content>
     </RdxDialog.Root>
   );
 }
